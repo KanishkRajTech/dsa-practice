6af45f6c5abce6d77ad1b5438e65516086694bcd
KANISHK RAJ <kanishkraj601@gmail.com>
2025-01-09 04:22:39 +0530

    Circular_linked_list_operations.c

---
+++ Linkedlist/Circular_linked_list_operations.c
@@ -0,0 +1,16 @@
+// Problem Statement:
+// -> Write a C program to implement a circular linked list with the following functionalities:
+
+// 1. Insert at the Beginning
+// 2. Insert at the End
+// 3. Insert at a Given Location
+// 4. Delete from the Beginning
+// 5. Delete from the End
+// 6. Delete from a Specified Position
+// 7. Reverse the Linked List
+// 8. Display the Linked List
+
+// Input/Output Requirements:
+// -> The program should display a menu for the user to choose the desired operation.
+// -> For insertion and deletion at a specific location, prompt the user to enter the position.
+// -> After every operation, the updated linked list should be displayed.
