d4ed1da155b6c995e8b3d398ed0b316e29236deb
KANISHK RAJ <kanishkraj601@gmail.com>
2025-01-11 17:42:03 +0530

    Doubly_linked_list_operations.c

--- Linkedlist/Doubly_linked_list_operations.c
+++ Linkedlist/Doubly_linked_list_operations.c
@@ -16,14 +16,6 @@
 // -> After every operation, the updated linked list should be displayed.
 
 
-/******************************************************************************
-
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
-Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
 #include <stdio.h>
 #include <stdlib.h>
 struct node{
